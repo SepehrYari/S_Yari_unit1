43c3582fa47d9cf1c95f4b1ca2b41317433cf47d
SepehrYari <91702741+SepehrYari@users.noreply.github.com>
2023-09-02 16:51:00 -0700

    Lesson D changes

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    public float Speed = 15.0f;
+    //rate of back and forth motion speed
+    private float Speed = 15.0f;
+    private float TurnSpeed = 15.0f;
+
+    //Input Variables
+    private float horizontalInput;
+    private float VerticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +21,12 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
         Debug.Log(Time.deltaTime);
-        transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+        //moving the car
+        transform.Translate(Vector3.forward * Time.deltaTime * Speed * VerticalInput);
+        //turning the car
+        transform.Rotate(Vector3.up, Time.deltaTime * TurnSpeed * horizontalInput);
     }
 }
